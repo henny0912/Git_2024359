661f179b350e6de115783abf25353038e533ec27
henny0912 <2024359@student.cct.ie>
2024-12-01 16:03:04 +0000

    Empty project

---
+++ src/git_2024359/Git_2024359.java
@@ -0,0 +1,25 @@
+/**
+ * Simple Git Test
+ * Egshiglen Enkhbayar - 2024359
+ */
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024359;
+
+/**
+ *
+ * @author egshi
+ */
+public class Git_2024359 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
